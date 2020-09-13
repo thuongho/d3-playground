eb1d375d098c8473dab4a591ded5dcb569ff47bb
Thuong Ho <thuong.t.ho@gmail.com>
2020-09-13 13:45:08 -0700

    Update scales on BarChart component

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -4,8 +4,18 @@ import * as d3 from "d3";
 import Bar from "./Bar";
 import Axis from "../Axis/Axis";
 
+// TODO: add interface for data and props
+
+/**
+ * Default configs
+ * TODO: move configs here
+ * potentially using useState to allow more control in tweaking these attributes
+ */
+const config = {};
+
 /**
  * Generates a component using d3 calculations
+ * TODO: fix JSDoc as the params are props
  * @function BarChart
  * @param {Number} x - x coord for starting point
  * @param {Number} y - y coord for starting point
@@ -14,6 +24,7 @@ import Axis from "../Axis/Axis";
  * @param {Object[]} data - dataset for charting
  * @param {Number} axisMargins - margins for axis
  * @param {String} color - color for bar
+ * @param {Number} barPadding - spacing between bars
  * @returns Rendered BarChart
  */
 const BarChart = ({ x, y, width, height, data, axisMargins, color }) => {
@@ -23,11 +34,23 @@ const BarChart = ({ x, y, width, height, data, axisMargins, color }) => {
    * @function xScale
    * @param {Number} x - value of x
    * @returns calculated width
+   * -------------------------------
+   * .scaleBand - divides domain evenly (length) or continuous range [start, end]
+   * .domain - dataset range used to map to chart range
+   * .range - chart range converts to pixels
+   * .paddingInner [0,1] - padding between the bars
+   * .paddingOuter [0,1] - padding outside the bar to leave room for axis legend
+   * .align [0,1] - 0 left, 0.5 center, 1 right
+   * .round boolean - rounds bandwidth
    */
   const xScale = d3
     .scaleBand()
     .domain(data.map((d) => d.x))
-    .range([0, width]);
+    .range([0, width])
+    .paddingInner(0.5)
+    .paddingOuter(0.5)
+    .align(0.5)
+    .round(true);
 
   /**
    * Vertical scale used to calc bar height
@@ -35,28 +58,37 @@ const BarChart = ({ x, y, width, height, data, axisMargins, color }) => {
    * @function yScale
    * @param {Number} y - value of y
    * @returns calculated height
+   * -----------------------------
+   * .scaleLinear - linear relationship between input and output
+   * .domain - dataset range used to map to chart range
+   *           extent gives the full array from min to max and nice rounds it off
+   * .range - chart range converts to pixels
+   *          pixels start from (0,0) so it is inverted
+   *          take chart height - margin bottom to margin top to get range
    */
   const yScale = d3
     .scaleLinear()
-    .domain([d3.max(data, (d) => d.y), 0])
-    .range([0, height]);
+    .domain(d3.extent(data , d => d.y)).nice()
+    .range([height - axisMargins.bottom, axisMargins.top]);
 
-  // TEST TO BE REMOVED!!
-  data.forEach((d) => console.log(yScale(d.y)));
+  /**
+   * Render a group of Bar components from the dataset
+   */
+  const renderedBars = data.map((item, i) => (
+    <Bar
+      key={`bar-${i}`}
+      x={xScale(item.x) + axisMargins.left}
+      y={height - yScale(item.y) + axisMargins.top}
+      width={xScale.bandwidth()}
+      height={yScale(item.y)}
+      color={color}
+    />
+  ));
 
   return (
     <g className="bar-chart" transform={`translate(${x}, ${y})`}>
       <g className="bars">
-        {data.map((item, i) => (
-          <Bar
-            key={`bar-${i}`}
-            x={xScale(item.x) + axisMargins.left}
-            y={height - yScale(item.y) + axisMargins.top + axisMargins.bottom}
-            width={xScale.bandwidth()}
-            height={yScale(item.y)}
-            color={color}
-          />
-        ))}
+        {renderedBars}
       </g>
       <Axis
         x={axisMargins.left}
